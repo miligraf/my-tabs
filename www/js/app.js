2e217027430586d689bb15f4f85759d0b629cbba
Milton Andrade <miligraf@gmail.com>
2016-08-09 13:03:34 -0400

    basic tabs app with home and settings, each has it's own view history

---
+++ www/js/app.js
@@ -0,0 +1,48 @@
+(function() {
+
+var app = angular.module('starter', ['ionic']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: '/home',
+    views: {
+      'tab-home': {
+        templateUrl: 'templates/home.html'
+      }
+    }
+  });
+
+  $stateProvider.state('details', {
+    url: '/details',
+    views: {
+      'tab-home': {
+        templateUrl: 'templates/details.html'
+      }
+    }
+  });
+
+  $stateProvider.state('settings', {
+    url: '/settings',
+    views: {
+      'tab-settings': {
+        templateUrl: 'templates/settings.html'
+      }
+    }
+  });
+
+  $urlRouterProvider.otherwise('/home');
+});
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+}());
